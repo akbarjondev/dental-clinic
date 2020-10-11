8789629518b0b0190430c1add65a84989a8a543b
AkbarjonTojiyev <tojiyev.akbarjon@gmail.com>
2020-10-11 22:56:10 +0500

    Make project ready

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// Burger Menu
+
+// var elButton = document.querySelector(".site-header__burger");
+// var elInner = document.querySelector(".nav-inner");
+
+// elButton.addEventListener("click", function() {
+// 	elInner.classList.toggle("nav-inner--toggle");
+// 	elButton.classList.toggle("site-header__burger--close");
+// });
+
+
+// noJS
+
+// var elMenu = document.querySelector(".menu");
+
+// elMenu.classList.remove("nojs");
