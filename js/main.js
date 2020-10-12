adb5290cf06c757abc02956aecbb11519681a5bd
AkbarjonTojiyev <tojiyev.akbarjon@gmail.com>
2020-10-12 12:56:26 +0500

    Add header

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 // Burger Menu
 
-// var elButton = document.querySelector(".site-header__burger");
-// var elInner = document.querySelector(".nav-inner");
+var elButton = document.querySelector(".nav__button");
+var elNavList = document.querySelector(".nav__list");
+var elSocialList = document.querySelector(".nav__socials-list");
 
-// elButton.addEventListener("click", function() {
-// 	elInner.classList.toggle("nav-inner--toggle");
-// 	elButton.classList.toggle("site-header__burger--close");
-// });
+elButton.addEventListener("click", function() {
+	elButton.classList.toggle("nav__button--close");
+	elSocialList.classList.toggle("nav__socials-list--toggle");
+	elNavList.classList.toggle("nav__list--toggle");
+});
 
 
 // noJS
